736000a7a69000360e01b51e29a1655e5078934a
adriana <acb2769@gmail.com>
2025-10-07 22:14:55 -0300

    operacao realizada em sala

---
+++ exercicios/respostas_exercicios_introdutorios_md.js
@@ -0,0 +1,69 @@
+// 1. Se ontem foi quarta-feira, amanhã será... 
+console.log('1')
+let array = ['Domingo', 'segunda-feira', 'terca-feira', 'quarta-feira', 'quinta-feira', 'sexta-feira', 'sabado']
+console.log('Se ontem foi quarta-feira, amanhã será... ')
+console.log('sexta-feira')
+
+
+
+/*
+console.log(array[0]) //domingo
+console.log(array[1]) //segunda
+console.log('O numero de itens do meu array é: ' + array.length) 
+//o tamanho do meu array -1 sempre será o tamanho máximo do meu array
+console.log('índice máximo do array ' + Number(array.length))
+
+let email= ' daniel@gmail.com.br   '
+
+if (email.trim() =='daniel@gmail.com')
+{
+console.log('Email com trim '+ '"'+ email.trim() + '"')
+console.log('Email válido!')
+}
+else
+{console.log('E-mail inválido!')
+
+}
+*/
+
+
+//2. Complete a sequência: 1, 4, 9, 16, 25, ...
+console.log(" ")
+console.log('2')
+console.log('complete a sequencia')
+console.log('1, 4, 9, 16, 25, ...', 6*6, 7*7)
+
+
+//3. Um trem de 100 metros de comprimento viaja a 50 km/h. Quanto tempo levará para atravessar um túnel de 50 metros?
+
+
+//4. Júlia é mais alta que Ana. Ana é mais baixa que Bruno. Qual dos três é o mais alto? Julia
+//5. Em uma sala, há 2 pais e 2 filhos. Qual o número mínimo de pessoas na sala? 3 pessoas
+
+//6. Se o código para "GATO" é 4130, qual é o código para "PATO"?
+
+//7. Você tem 50 moedas de 1 real e 50 moedas de 50 centavos. No total, quanto você tem?
+
+console.log(" ")
+console.log('7')
+let moedas = (50*1 + 50*0.5)
+console.log('Total de moedas:', moedas)
+
+//8. Uma torneira pinga 1 gota a cada segundo. Quantas gotas ela pingará em 1 minuto?
+
+console.log(" ")
+console.log('8')
+let gotas = (60*1)
+console.log('Total de gotas em 1 minuto:', gotas)
+
+//9. Se todos os corvos são pretos e minha caneta é um corvo, então minha caneta é preta. Essa afirmação é logicamente válida?
+//10. Se 3 coelhos comem 3 cenouras em 3 minutos, quanto tempo 10 coelhos levarão para comer 10 cenouras? 3 minutos
+//11. Se "SOL" está para "LUZ" como "GELO" está para...
+//12. João, Pedro e Maria têm 3 profissões diferentes: médico, professor e engenheiro. João não é médico. Pedro é amigo do engenheiro. 
+// Quem é o professor?
+//13. Uma caixa de bombons custa 10 reais. Se a caixa custa 8 reais a mais que os bombons, qual o preço dos bombons?
+console.log(" ")
+console.log('13')
+let caixa = 10
+let bombom = 10
+
